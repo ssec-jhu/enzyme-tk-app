214830eed192463bbbeb23fe53a50126b3fa8e4d
Fatemeh <1739327+fabbasinejad@users.noreply.github.com>
2026-06-24 14:44:31 -0400

    removing old icons

--- enzyme_tk_app/app/components/icons.py
+++ enzyme_tk_app/app/components/icons.py
@@ -11,20 +11,14 @@ ICON_LOGO = "fa-solid fa-flask"
 ICON_TOOL_REACTION = "fa-solid fa-flask-vial"
 ICON_TOOL_SEQUENCE = "fa-solid fa-dna"
 ICON_TOOL_STRUCTURE = "fa-solid fa-cube"
-ICON_TOOL_TBD = "fa-solid fa-gears"
 ICON_TOOL_TIMER = "fa-solid fa-hourglass-half"
 ICON_DATA_WARNING = "fa-solid fa-triangle-exclamation"
-
-# --- Hero Feature Pills ---
-ICON_HERO_PYTHON = "fa-brands fa-python"
-ICON_HERO_ASYNC = "fa-solid fa-bolt"
 
 # --- Footer: Social Links ---
 ICON_SOCIAL_GITHUB = "fa-brands fa-github"
 
 # --- Tool Modals ---
 ICON_MODAL_EXAMPLE = "fa-solid fa-lightbulb"
-ICON_MODAL_DATABASE = "fa-solid fa-database"
 ICON_MODAL_UPLOAD = "fa-solid fa-upload"
 
 # --- Modal Section Headers ---
@@ -37,7 +31,6 @@ ICON_JOB_CANCEL = "fa-solid fa-ban"
 ICON_JOB_VIEW = "fa-solid fa-eye"
 ICON_JOB_CLEAR = "fa-solid fa-trash-can"
 ICON_JOB_CANCEL_ALL = "fa-solid fa-xmark"
-ICON_JOB_REFRESH = "fa-solid fa-arrows-rotate"
 ICON_JOB_BACK = "fa-solid fa-arrow-left"
 
 # --- Job Status Icons ---
@@ -52,7 +45,6 @@ ICON_STATUS_TIMEOUT = "fa-solid fa-hourglass-end"
 ICON_ADMIN_PAGE = "fa-solid fa-user-shield"
 ICON_ADMIN_LOCK = "fa-solid fa-lock"
 ICON_ADMIN_SESSIONS = "fa-solid fa-users"
-ICON_ADMIN_REFRESH = "fa-solid fa-arrows-rotate"
 ICON_ADMIN_CANCEL_SELECTED = "fa-solid fa-ban"
 ICON_ADMIN_CLEAR = "fa-solid fa-broom"
 ICON_ADMIN_PURGE = "fa-solid fa-skull-crossbones"
